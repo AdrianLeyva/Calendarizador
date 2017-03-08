47ab2b6b9373b26af07065dc12375c36817d8eb9
Armando <armandoecr_96@hotmail.com>
2017-03-07 21:05:07 -0600

    Desarrollo clase RoundRobin

--- src/controller/RoundRobin.java
+++ src/controller/RoundRobin.java
@@ -14,37 +14,112 @@ import model.*;
  */
 public class RoundRobin {
     
+    private ArrayList<Proceso> processesList;
+    private boolean isCPURunning;
+    private Stack readyStack;
+    private Stack finishedStack;
     private Proceso currentProcess;
+    private int listSize;
+    private int timer;
     private int quantum;
-
-    public RoundRobin(Proceso currentProcess, int quantum) {
-        this.currentProcess = currentProcess;
-        this.quantum = quantum;
-    }
-
-    public Proceso getCurrentProcess() {
-        return currentProcess;
-    }
-
-    public void setCurrentProcess(Proceso currentProcess) {
-        this.currentProcess = currentProcess;
-    }
-
-    public int getQuantum() {
-        return quantum;
-    }
-
-    public void setQuantum(int quantum) {
+    
+    public RoundRobin(ArrayList<Proceso> processesList, int quantum) {
+        this.processesList = processesList;
+        this.isCPURunning = false;
+        this.readyStack = new Stack();
+        this.finishedStack = new Stack();
+        this.currentProcess = null;
+        this.listSize = this.processesList.size();
+        this.timer = 0;
         this.quantum = quantum;
     }
     
+    public void execute(){
+        sortProcessesList();
+        
+        while(this.finishedStack.size() != this.listSize){
+            addNewProcess();
+            runCPU();
+        }
+        
+        printFinishedStack();
+        printTimer();
+    }
     
-    public void roundRobin(int quantum){
+    
+    //Valida procesos entrantes y los agrega a la pila de listos si no son nulos.
+    private void addNewProcess(){
+        Proceso process = getProcessComing();
         
-        Queue<Proceso> cola = new LinkedList();
+        if(process!=null){
+            this.readyStack.push(process);
+        }
+    }
+    
+    private void runCPU(){
+        if(!this.isCPURunning){
+            this.currentProcess = (Proceso)this.readyStack.firstElement();
+            this.currentProcess.setTiempoEspera(this.timer - this.currentProcess.getTiempoLlegada());
+            this.readyStack.remove(0);
+            this.isCPURunning = true;
+        }else{
+            this.currentProcess.setTiempoTotal(this.timer - this.currentProcess.getTiempoLlegada());
+            
+            double dinamicRafaja = (this.currentProcess.getTiempoTotal() - this.currentProcess.getTiempoEspera());
         
+            if(this.currentProcess.getRafaga() == dinamicRafaja){
+                this.finishedStack.push(this.currentProcess);
+                this.isCPURunning = false;
+                this.timer--;
+            }
+            
+            
+            this.timer++;
+            
+            
+        }
         
-        
+    }
+    
+    
+    //Verifica si ha llegado un nuevo proceso.
+    private Proceso getProcessComing(){
+        Proceso process = null;
+        for(int i=0;i<this.processesList.size();i++){
+            if(this.processesList.get(i).getTiempoLlegada() == this.timer){
+                process = this.processesList.get(i);
+                this.processesList.remove(i);
+                break;
+            }
+        }
+        return process;
+    }
+    
+    private void sortProcessesList(){
+        Proceso buffer;
+        int i,j;
+        for(i = 0; i < processesList.size(); i++){
+            for(j = 0; j < i; j++){
+                if(processesList.get(i).getTiempoLlegada() < processesList.get(j).getTiempoLlegada())
+                {
+                    buffer = processesList.get(j);
+                    processesList.set(j, processesList.get(i));
+                    processesList.set(i, buffer);
+                }
+            }
+        }      
+    }
+    
+    private void printFinishedStack(){
+        int limit = this.finishedStack.size();
+        for(int i=0;i<limit;i++){
+            Proceso proceso = (Proceso)this.finishedStack.pop();
+            System.out.println(proceso.toString());
+        }
+    }
+    
+    private void printTimer(){
+        System.out.println("El tiempo total de ejecución es: " + this.timer);
     }
     
 }
